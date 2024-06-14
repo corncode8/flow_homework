e9089929a797614eb56f29d8cc08386ef2718ffe
corncode8 <myeongcheol8@naver.com>
2024-06-15 01:30:25 +0900

    View, Search, Delete, 시간대 출력 구현

---
+++ src/main/java/com/flow/homework/api/util/config/QuerydslConfig.java
@@ -0,0 +1,16 @@
+package com.flow.homework.api.util.config;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class QuerydslConfig {
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+}
